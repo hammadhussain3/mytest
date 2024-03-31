4eaa79ac18f53f1ca11d3f26cd8d68eea1d9f3f8
hammadhussain3 <dev.hammad@yahoo.com>
2024-03-31 11:18:56 +0500

    roles and permissions

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ Route::get('/admin/dashboard',[UserController::class,'admin_dashboard'])->name('
 Route::get('/editor/dashboard',[UserController::class,'editor_dashboard'])->name('editor_dashboard')->middleware('role:editor|admin');
 Route::get('/viewer/dashboard',[UserController::class,'viewer_dashboard'])->name('viewer_dashboard')->middleware('role:editor|admin|viewer');
 Route::get('/category',[CategoryController::class,'index'])->name('category');
-Route::get('/add/category',[CategoryController::class,'add_category'])->name('category.add');
+Route::get('/add/category',[CategoryController::class,'add_category'])->name('category.add')->middleware('permission:create');
 Route::post('/category/store',[CategoryController::class,'category_store'])->name('category.store');
-Route::get('/category/edit/{id}',[CategoryController::class,'edit_category'])->name('category.edit');
-Route::get('/category/view/{id}',[CategoryController::class,'view_category'])->name('category.view');
-Route::get('/category/delete/{id}',[CategoryController::class,'delete_category'])->name('category.delete');
+Route::get('/category/edit/{id}',[CategoryController::class,'edit_category'])->name('category.edit')->middleware('permission:update');
+Route::get('/category/view/{id}',[CategoryController::class,'view_category'])->name('category.view')->middleware('permission:read');
+Route::get('/category/delete/{id}',[CategoryController::class,'delete_category'])->name('category.delete')->middleware('permission:delete');
 Route::post('/category/update',[CategoryController::class,'update_category'])->name('update_category');
 });
 Route::get('/logout',[UserController::class,'logout'])->name('logout');
